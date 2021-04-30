24fc38906da6020e2ab2efcc240e3312974a99eb
aldosandov <aldosans2016@gmail.com>
2021-04-30 16:09:39 -0500

    requirements.txt added

--- api/src/db.py
+++ api/src/db.py
@@ -1,7 +1,7 @@
 import pymongo
 
 
-client = pymongo.MongoClient("mongodb+srv://Aldo:98WgdtkbH7dlxDys@cluster0.hk31r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://Aldo:<password>@cluster0.hk31r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.get_database('fraud_db')
 
 users = db.users
